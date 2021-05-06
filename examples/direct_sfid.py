4e92d41b473c03602a37b13ec9cb80644f9a2bf9
evenmn <evenmn@fys.uio.no>
2021-05-06 23:51:29 +0200

    Updated examples

--- examples/direct_sfid.py
+++ examples/direct_sfid.py
@@ -2,9 +2,12 @@ import torch
 import pytorch_sfid as ps
 
 N = 1000
+A = 1
+
 real_images = torch.rand(N, 1, 32, 32)
-real_attr = torch.rand((N, 3))
-fake_attr = torch.rand((N, 3))
+real_attr = torch.rand((N, A))
+fake_attr = torch.rand((N, A))
 
 ps.set_config(ncenters=3, radius=0.6, batch_size=32)
-sfid = ps.get_sfid(fake_images, fake_attr, real_images=real_images, real_attr=real_attr)
+sfid = ps.get_sfid(real_images, fake_attr, real_images=real_images, real_attr=real_attr, prnt=True)
+print("SFID value is: ", sfid)
