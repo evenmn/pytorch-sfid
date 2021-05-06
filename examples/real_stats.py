d82297378b3961c978db81e587b485618b7db9e4
evenmn <evenmn@fys.uio.no>
2021-05-06 22:06:56 +0200

    Added examples

---
+++ examples/real_stats.py
@@ -0,0 +1,11 @@
+import torch
+import pytorch_sfid as ps
+
+N = 1000
+real_images = torch.rand(N, 1, 32, 32)
+real_attr = torch.rand((N, 3))
+fake_attr = torch.rand((N, 3))
+
+ps.set_config(ncenters=3, radius=0.6, batch_size=32)
+real_stats = ps.get_stats(real_images, real_attr)
+sfid = ps.get_sfid(fake_images, fake_attr, real_stats=real_stats)
