223af78497e50087a0ae90cb9878c2dab3469786
evenmn <evenmn@mn.uio.no>
2024-04-23 21:04:59 +0200

    Minor

--- examples/real_stats.py
+++ examples/real_stats.py
@@ -1,14 +1,16 @@
 import torch
 import pytorch_sfid as ps
 
-N = 1000
-A = 1
+Nr = 1800
+Nf = 10000
+A = 3
 
-real_images = torch.rand(N, 1, 32, 32)
-real_attr = torch.rand((N, A))
-fake_attr = torch.rand((N, A))
+real_images = torch.rand(Nr, 1, 128, 128)
+fake_images = torch.rand(Nf, 1, 128, 128)
+real_attr = torch.rand((Nr, A))
+fake_attr = torch.rand((Nf, A))
 
-ps.set_config(ncenters=3, radius=0.6, batch_size=32)
-real_stats = ps.get_stats(real_images, real_attr)
-sfid = ps.get_sfid(real_images, fake_attr, real_stats=real_stats, prnt=True)
+ps.set_config(ncenters=6, radius=0.2, batch_size=8)
+real_stats = ps.get_stats(real_images, real_attr, prnt=True)
+sfid = ps.get_sfid(fake_images, fake_attr, real_stats=real_stats, prnt=True)
 print("SFID value is: ", sfid)
